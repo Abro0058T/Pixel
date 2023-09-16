f2786b92edf4c5275a3e7d2238db7c43c4d65737
AbhiAtiUttam <Abhi@glocalize.net>
2023-09-16 14:48:25 +0530

    detection feature improved

--- services/detect.py
+++ services/detect.py
@@ -1,40 +1,57 @@
-import requests
-from bs4 import BeautifulSoup
 
-# Define the URL of the website you want to check
+# Importing libraries
+import time
+import hashlib
+from urllib.request import urlopen, Request
 
-def detect():
-    url = 'https://khatri-nipun.netlify.app/'
+# setting the URL you want to monitor
+url = Request('https://benevolent-belekoy-1387de.netlify.app',
+			headers={'User-Agent': 'Mozilla/5.0'})
 
-# Define a file to store the previous version of the webpage content
-    previous_content_file = 'previous_content.html'
-    
-    # Send a GET request to the URL and retrieve the current webpage content
-    response = requests.get(url)
-    current_content = response.text
-    
-    # Try to open the file containing the previous version of the webpage content
-    try:
-        with open(previous_content_file, 'r', encoding='utf-8') as file:
-            previous_content = file.read()
-    except FileNotFoundError:
-        previous_content = None
-    
-    # If the file didn't exist or the current content is different from the previous content, update the file and extract data
-    if previous_content is None or current_content != previous_content:
-        print("The website has been updated!")
-    
-        # Extract data using BeautifulSoup (you can modify this part according to your needs)
-        soup = BeautifulSoup(current_content, 'html.parser')
-        
-        # Example: Extract all the links from the updated webpage
-        links = [a['href'] for a in soup.find_all('a')]
-        print(links)
-        # Save the current content as the new previous content
-        with open(previous_content_file, 'w', encoding='utf-8') as file:
-            file.write(current_content)
-    else:
-        print("The website has not been updated.")
-    
-detect()
+# to perform a GET request and load the
+# content of the website and store it in a var
+response = urlopen(url).read()
 
+# to create the initial hash
+currentHash = hashlib.sha224(response).hexdigest()
+print("running")
+time.sleep(10)
+while True:
+	try:
+		# perform the get request and store it in a var
+		response = urlopen(url).read()
+
+		# create a hash
+		currentHash = hashlib.sha224(response).hexdigest()
+
+		# wait for 30 seconds
+		time.sleep(30)
+
+		# perform the get request
+		response = urlopen(url).read()
+
+		# create a new hash
+		newHash = hashlib.sha224(response).hexdigest()
+
+		# check if new hash is same as the previous hash
+		if newHash == currentHash:
+			continue
+
+		# if something changed in the hashes
+		else:
+			# notify
+			print("something changed")
+
+			# again read the website
+			response = urlopen(url).read()
+
+			# create a hash
+			currentHash = hashlib.sha224(response).hexdigest()
+
+			# wait for 30 seconds
+			time.sleep(30)
+			continue
+
+	# To handle exceptions
+	except Exception as e:
+		print("error")
