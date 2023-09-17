fa588bd069eec024c142cbdfe33dee53bffc4daa
Ashish Kushwaha <ashishkushwahacb@gmail.com>
2023-09-17 16:52:36 +0530

    added /api prefix to all routes

--- routes/base.py
+++ routes/base.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from routes import user
 from routes import video
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 router.include_router(user.router)
 router.include_router(video.router)
